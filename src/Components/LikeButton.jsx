dc676bafbda732f66021219d51d9e0fd8c1dfbbc
Lena Cortes <lena.cortes@usal.es>
2024-12-11 14:05:41 +0100

    iteration1 done

---
+++ src/Components/LikeButton.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+function LikeButton () {
+
+    // Declare a new state variable, which we'll call "count"
+    const [count, setCount] = useState(0);
+
+    return(<button onClick={() => setCount(count + 1)}>{ count } Likes</button>)
+}
+
+export default LikeButton;
